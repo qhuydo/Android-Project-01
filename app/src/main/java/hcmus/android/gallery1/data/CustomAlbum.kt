550bd42cc0f0a0fcc67106973e7e2714f5663563
qhuydo <46428103+qhuydo@users.noreply.github.com>
2021-09-01 21:34:47 +0700

    Move CustomAlbum data objects into one file

--- app/src/main/java/hcmus/android/gallery1/data/CustomAlbum.kt
+++ app/src/main/java/hcmus/android/gallery1/data/CustomAlbum.kt
@@ -1,8 +1,6 @@
 package hcmus.android.gallery1.data
 
-import androidx.room.Embedded
-import androidx.room.Junction
-import androidx.room.Relation
+import androidx.room.*
 
 data class CustomAlbum(
     @Embedded val albumInfo: CustomAlbumInfo,
@@ -18,4 +16,61 @@ data class CustomAlbum(
         entity = CustomAlbumItem::class,
     )
     val albumItems: List<CustomAlbumItem>
+)
+
+@Entity(
+    tableName = "custom_album_cross_ref",
+    primaryKeys = ["album_id", "item_id"],
+    foreignKeys = [
+        ForeignKey(
+            entity = CustomAlbumInfo::class,
+            parentColumns = ["id"],
+            childColumns = ["album_id"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = CustomAlbumItem::class,
+            parentColumns = ["id"],
+            childColumns = ["item_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class CustomAlbumCrossRef(
+    @ColumnInfo(name = "album_id")
+    val albumId: Long,
+
+    // Id of the MediaItem
+    // retrieved from BaseColumns._ID column in MediaStore
+    @ColumnInfo(name = "item_id", index = true)
+    val itemId: Long
+)
+
+@Entity(tableName = "custom_album_info")
+data class CustomAlbumInfo(
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0L,
+
+    val name: String,
+
+    @ColumnInfo(name = "date_created")
+    var dateCreated: Long = System.currentTimeMillis(),
+
+    //
+    // @ColumnInfo(name = "date_modified")
+    // var dateModified: Long = System.currentTimeMillis(),
+
+    // Id of the MediaItem to be used as a thumbnail of the album
+    // retrieved from BaseColumns._ID column in MediaStore
+    @ColumnInfo(name = "thumbnail_id")
+    var thumbnailId: Long = -1 // -1: not assigning value yet
+)
+
+@Entity(tableName = "custom_album_item")
+data class CustomAlbumItem(
+    // Id of the MediaItem
+    // retrieved from BaseColumns._ID column in MediaStore
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: Long,
 )
