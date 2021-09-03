1973df2d9ef2f2677df6927c56b415687f847612
qhuydo <46428103+qhuydo@users.noreply.github.com>
2021-09-03 20:57:55 +0700

    Add `PhotoRepository`

---
+++ app/src/main/java/hcmus/android/gallery1/repository/PhotoRepository.kt
@@ -0,0 +1,52 @@
+package hcmus.android.gallery1.repository
+
+import hcmus.android.gallery1.data.DataSource
+import hcmus.android.gallery1.data.Item
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.withContext
+
+interface PhotoRepository {
+    suspend fun getAllPhotos(): List<Item>
+    suspend fun getItemsFromCollection(collectionId: Long): List<Item>
+    suspend fun getItemFromDateCollection(timeInMillis: Long): List<Item>
+}
+
+class PhotoRepositoryImpl(
+    private val mediaStoreSource: DataSource
+) : PhotoRepository {
+
+    companion object {
+        @Volatile
+        private var INSTANCE: PhotoRepository? = null
+
+        fun getInstance(
+            mediaStoreSource: DataSource
+        ): PhotoRepository {
+
+            return INSTANCE ?: synchronized(this) {
+                PhotoRepositoryImpl(
+                    mediaStoreSource
+                ).also {
+                    INSTANCE = it
+                }
+            }
+        }
+    }
+
+    private val scope = Job() + Dispatchers.IO
+
+    override suspend fun getAllPhotos(): List<Item> = withContext(scope) {
+        return@withContext mediaStoreSource.getItems()
+    }
+
+    override suspend fun getItemsFromCollection(collectionId: Long): List<Item> =
+        withContext(scope) {
+            return@withContext mediaStoreSource.getItems(collectionId)
+        }
+
+    override suspend fun getItemFromDateCollection(timeInMillis: Long): List<Item> =
+        withContext(scope) {
+            return@withContext mediaStoreSource.getItemsByDate(timeInMillis)
+        }
+}
